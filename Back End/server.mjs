dabb28be9360d2544a91e26ab8b51ae76845c74a
Hassannadeem861 <Professionalwebdeveloper123@gmail.com>
2023-12-15 17:49:02 +0500

    Working

--- Back End/server.mjs
+++ Back End/server.mjs
@@ -1,26 +1,26 @@
 console.log(`MERN STACK IS RUNNING`);
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import cors from "cors";
-
-
-
 
 const app = express();
 dotenv.config();
 app.use(cors());
-
-
+app.use(express.json()); // body parser
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).send("Hello World!");
 });
 
 app.get("/api/v1/linkedin", (req, res, next) => {
- res.send("Linkedin Page")
+  res.status(200).send("This is linkedin page");
 });
 
-const PORT = process.env.PORT || 3000;
+app.get("/api/v1/about", (req, res, next) => {
+  res.status(200).send(`This is about page`);
+});
+
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example server listening on port ${PORT}`);
 });
